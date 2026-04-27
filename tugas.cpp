c7fae82d54e3c80312d15b40b5cd5487cc70b9be
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-27 17:51:14 +0700

    menambahkan method inputData() pada class LayangLayang

--- tugas.cpp
+++ tugas.cpp
@@ -10,3 +10,12 @@ private:
 //Lengkapi disini
 double d1, d2;
 double sisi1, sisi2;
+
+public:
+void inputData() {
+cout << "Input Layang-Layang\n";
+cout << "Diagonal 1: "; cin >> d1;
+cout << "Diagonal 2: "; cin >> d2;
+cout << "Sisi 1: "; cin >> sisi1;
+cout << "Sisi 2: "; cin >> sisi2;
+}
